9a21c5702b68d9e5b1c814974862a58a33717042
ueno takumi <namakemono1911@gmail.com>
2018-09-15 23:07:42 +0900

    透視投影変換でレイを飛ばしてヒット判定取れるようにした GunSettingクラスを追加して銃の設定をインスペクター上からできるようにした

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,10 +7,29 @@ public class ControlSetting
 {
     public KeyCode shotButton;         //発射ボタン
     public KeyCode reloadButton;       //リロードボタン
+    public KeyCode guardButton;        //ガードボタン
     public float mouseSensitivity;     //感度
     public string axisNameX;           //レティクルの横の動き
     public string axisNameY;           //レティクルの縦の動き
-    public RectTransform reticle;      //レティクル情報
+    public GameObject reticle;         //レティクル情報
+}
+
+[System.Serializable]
+public class GunSetting
+{
+    public int numBullet;           //装弾数
+    public int remBullet;           //残弾数
+    public float reloadTime;        //リロード時間
+
+    public void shoot()
+    {
+        remBullet--;
+    }
+
+    public void reload()
+    {
+        remBullet = numBullet;
+    }
 }
 
 public class playerController : MonoBehaviour {
@@ -18,11 +37,19 @@ public class playerController : MonoBehaviour {
     [SerializeField]
     private ControlSetting control;     //プレイヤーのコントロール
 
+    [SerializeField]
+    private GunSetting gun;             //銃の設定
+
     private playerState state;          //プレイヤーのステートパターン
 
     public ControlSetting Control       //コントロール取得
     {
         get { return control; }
+    }
+
+    public GunSetting Gun
+    {
+        get { return gun; }
     }
 
 
@@ -36,6 +63,12 @@ public class playerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //レティクル移動
+        Vector3 reticlePos = Vector3.zero;
+        reticlePos.x = Input.GetAxis(control.axisNameX) * control.mouseSensitivity;
+        reticlePos.y = Input.GetAxis(control.axisNameY) * control.mouseSensitivity;
+        control.reticle.transform.position += reticlePos;
+
         state.updateState();
 	}
 
--- Assets/Scripts/playerDefault.cs
+++ Assets/Scripts/playerDefault.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerDefault : playerState {
+
+    private Canvas canvas;
 
     public playerDefault(playerController p) : base(p) { }
 
 	// Use this for initialization
 	void Start () {
-		
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
 	}
 
     public override void initState()
@@ -18,11 +21,49 @@ public class playerDefault : playerState {
 
     public override void updateState()
     {
-        Vector3 reticlePos = Vector3.zero;
+        //射撃
+        if (Input.GetKeyDown(player.Control.shotButton))
+        {
+            shootGun();
+        }
 
-        reticlePos.x = Input.GetAxis(player.Control.axisNameX) * player.Control.mouseSensitivity;
-        reticlePos.y = Input.GetAxis(player.Control.axisNameY) * player.Control.mouseSensitivity;
+        //ガード
+        if (Input.GetKeyDown(player.Control.guardButton))
+        {
 
-        player.Control.reticle.position += reticlePos;
+        }
+
+        //リロード
+        if (Input.GetKeyDown(player.Control.reloadButton))
+        {
+
+        }
+    }
+
+    //射撃
+    void shootGun()
+    {
+        //射撃処理
+        player.Gun.shoot();
+
+        //透視投影変換
+        var screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, player.Control.reticle.transform.position);
+        var pos = Vector3.zero;
+        var canvasRect = GameObject.Find("Canvas").GetComponent<RectTransform>();
+
+        var ray = RectTransformUtility.ScreenPointToRay(Camera.main, screenPos);
+
+        //当たり判定
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "enemy")
+                Destroy(hit.transform.gameObject);
+        }
+
+        //デバッグ表示
+        var line = GameObject.Find("debugLine").GetComponent<LineRenderer>();
+        line.SetPosition(0, ray.origin);
+        line.SetPosition(1, ray.direction * 100 + Camera.main.transform.position);
     }
 }
