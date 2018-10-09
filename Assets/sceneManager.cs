6f3d7d74cbd1fb6ce9f32aa7f1cd987b9c70cfa7
taktyeMoti <hal.ishiitakahiro@gmail.com>
2018-10-09 13:56:04 +0900

    訳あって先にシーンマネージャ作った

---
+++ Assets/sceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public void SceneChange(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+}
---
+++ Assets/sceneManagerInstancer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneManagerInstancer : MonoBehaviour {
+
+	[SerializeField]
+	GameObject sceneManager;
+
+	private void Awake()
+	{
+		GameObject gm;
+		gm = GameObject.Find("sceneManager");
+
+		if(gm == null)
+		{
+			GameObject.Instantiate(sceneManager);
+		}
+
+		Destroy(this.gameObject);
+
+	}
+}
