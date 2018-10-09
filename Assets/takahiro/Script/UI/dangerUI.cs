3dc76c929985d83343e7fb16229a72477f01265f
taktye <taktye_tsoyo_basket08@yahoo.co.jp>
2018-10-09 22:49:12 +0900

    敵弾の警告、被弾表現、残機表記完成。簡単すぎて一つ一つコミットするの忘れてました。すみません。 付属テキストに説明あり。

---
+++ Assets/takahiro/Script/UI/dangerUI.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dangerUI : MonoBehaviour
+{
+	[SerializeField]
+	private Transform testTarnsform;			// テスト用の3d座標
+	[SerializeField]
+	private Image dangetUI;                     // ui
+	[SerializeField]
+	private int visibleTimeStart;               // 一番遠い位置での点滅間隔(フレーム)
+	[SerializeField]
+	private int visibleTimeEnd;                 // 一番近い位置点滅間隔(フレーム)
+
+	private Transform tr;						// 表示位置
+	private float length;						// 弾との距離(リアルタイム)
+	private float lengthStand;					// 弾との距離(撃った初期位置)
+	private bool visible;						// 表示、非表示
+	private bool isCount;						// 表示切替か否か
+	private Vector3 playerPos;					// プレイヤーの位置
+	private int visibleTimeDifference;			// 点滅間隔の差分
+	private int timeCount;						// タイマー
+	private int visibleTime;                    // 切り替わる時間
+												//	private (弾) bullet;					// 弾から位置貰う
+
+
+	private void Start()
+	{
+		playerPos = new Vector3(0, 0, 0);
+		lengthStand = 10;
+		visibleTimeDifference = visibleTimeStart - visibleTimeEnd;
+		isCount = false;
+		visible = true;
+	}
+
+	private void Update()
+	{
+		if (isCount)
+		{
+			++timeCount;
+			if (timeCount >= visibleTime)
+			{
+				timeCount = 0;
+				isCount = false;
+			}
+		}
+		else
+		{
+			isCount = true;
+
+			Color cr = dangetUI.color;
+
+			if(visible)
+			{
+				cr.a = 1.0f;
+			}
+			else
+			{
+				cr.a = 0.0f;
+			}
+
+			dangetUI.color = new Color(cr.r, cr.g, cr.b, cr.a);
+			visible = !visible;
+
+			// 今の位置
+			Vector3 bulletPos = testTarnsform.position;
+			//bulletPos = bullet.GetPos();    // 弾の位置取得
+													// 距離の計算
+			length = Vector3.Distance(bulletPos, playerPos);
+			// 今の距離の初めの距離に対する割合
+			float perLength = length / lengthStand;
+			// 差分の大きさ
+			int dif = (int)((float)visibleTimeDifference * perLength);
+			// 最終的な点滅間隔
+			visibleTime = visibleTimeEnd + dif;
+
+		}
+	}
+
+	// 初期長さのセット
+	public void setLength(float l)
+	{
+		lengthStand = l;
+	}
+
+	// 位置セット
+	public void setPos(Transform t)
+	{
+		tr = t;
+	}
+
+	// プレイヤーの位置セット
+	public void setPlayerPos(Vector3 pos)
+	{
+		playerPos = pos;
+	}
+
+	// 敵の弾セット
+	//public void setEnemyBullet((敵の弾型) bt)
+	//{
+	//	bullet = bt;
+	//}
+}
---
+++ Assets/takahiro/Script/UI/playerLifeUI.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerLifeUI : MonoBehaviour {
+
+	[SerializeField]
+	private int lifeMax;			// 最大値 0で無限
+	[SerializeField]
+	private int lifeFirst;			// 初期値
+	[SerializeField]
+	private GameObject lifeImage;		// ライフイメージ
+	[SerializeField]
+	private float gapPos;			// ずれ
+	[SerializeField]
+	private bool is1Player;			// 1pか2pか
+	[SerializeField]
+	private float gapSize;			// 1つめと2つめ以降のサイズ比率
+
+	private int lifeNow;        // 現在のライフ
+	private Vector2 lifeSizeDelta;	// 2ライフ目以降のサイズ
+
+	// Use this for initialization
+	void Start () {
+		lifeNow = lifeFirst;
+		lifeSizeDelta = lifeImage.GetComponent<RectTransform>().sizeDelta;
+		lifeSizeDelta *= gapSize;
+		SetUI(lifeNow);
+	}
+
+	private void SetUI(int life)
+	{
+		// 全除去
+		if (this.transform.childCount > 0)
+		{
+			int child = this.transform.childCount;
+			for (int i = 0; i < child; ++i)
+			{
+				Destroy(this.transform.GetChild(i).gameObject);
+			}
+		}
+
+		// ライフ確認
+		if (isDeth())
+		{
+			return;
+		}
+
+		// 1つ目を配置
+		GameObject.Instantiate(lifeImage, this.transform.position, Quaternion.identity, this.transform);
+
+		// 2つ目以降
+		GameObject childObj;
+		Vector3 childPos = this.transform.position;
+		for (int i = 1; i < getPlayerLife(); ++i)
+		{
+			if (is1Player)
+			{
+				childPos.x -= gapPos;
+			}
+			else
+			{
+				childPos.x += gapPos;
+			}
+			childPos.z -= 0.1f;
+
+			childObj = GameObject.Instantiate(lifeImage, childPos, Quaternion.identity, this.transform);
+			childObj.GetComponent<RectTransform>().sizeDelta = new Vector2(lifeSizeDelta.x, lifeSizeDelta.y);
+		}
+	}
+
+	// ダメージor回復
+	public void AddPlayerLife(int n)
+	{
+		lifeNow += n;
+
+		if(lifeMax > 0)
+		{
+			if(lifeNow >= lifeMax)
+			{
+				lifeNow = lifeMax;
+			}
+		}
+
+		if(lifeNow <= 0)
+		{
+			lifeNow = 0;
+		}
+
+		SetUI(lifeNow);
+
+	}
+
+	// 死亡確認
+	public bool isDeth()
+	{
+		if(lifeNow <= 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	// 残基確認
+	public int getPlayerLife()
+	{
+		return lifeNow;
+	}
+}
