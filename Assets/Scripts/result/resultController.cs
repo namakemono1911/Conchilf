af2216c500c2647b4db40f3db2cd82d8f32250a2
ueno <namakemono1911@gmail.com>
2018-10-18 13:00:32 +0900

    仕様が決まったので仕様に沿ったプログラムを実装中

---
+++ Assets/Scripts/result/resultController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class resultPair
+{
+    public string resultName;
+    public Text numText;
+    public Text scoreText;
+}
+
+public class resultController : MonoBehaviour
+{
+    [SerializeField]
+    private resultPair[] pair;
+
+    private Dictionary<string, Text> dict;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
