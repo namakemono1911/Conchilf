3a180b90500b9ca7672c6a098ee747615ff56d86
taktyeMoti <hal.ishiitakahiro@gmail.com>
2018-10-22 14:28:00 +0900

    ゲーム中用のカメラ移動仮完成

---
+++ Assets/takahiro/Script/Camera/cameraMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+
+	[SerializeField]
+	private virtualCameraSet[] cameraSet;
+	[SerializeField]
+	private int setNum;
+
+	private float speed;
+	private float now;
+	// Use this for initialization
+	void Start () {
+		speed = 0.01f * cameraSet[setNum].getSpeed();
+		Debug.Log((float)cameraSet[setNum].getNumWaypoints());
+		now = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(setNum >= cameraSet.Length)
+		{
+			setNum = 0;
+		}
+
+		transform.position = cameraSet[setNum].getcameraPath().EvaluatePositionAtUnit(now, Cinemachine.CinemachinePathBase.PositionUnits.Distance);
+		now += speed;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			nextMove();
+
+	}
+
+	public void nextMove()
+	{
+		setNum += 1;
+
+		if (setNum >= cameraSet.Length)
+		{
+			setNum = 0;
+		}
+
+		speed = 0.01f * cameraSet[setNum].getSpeed();
+		now = 0.0f;
+	}
+}
