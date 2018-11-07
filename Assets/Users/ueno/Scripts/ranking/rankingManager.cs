ecf5e05ec1868ca2f4cee9ac5452490548a52930
ueno <namakemono1911@gmail.com>
2018-11-07 16:58:56 +0900

    途中

---
+++ Assets/Users/ueno/Scripts/ranking/rankingManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankData
+{
+    public string name;
+    public uint score;
+}
+
+public class rankingManager : MonoBehaviour
+{
+    private static List<RankData> rankDatas;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //ランクインしてるかどうか
+    public bool whetherRankin(RankData data)
+    {
+        foreach (var rank in rankDatas)
+        {
+            if (rank.score >= data.score)
+                return true;
+        }
+
+        return false;
+    }
+
+    //ランキングデータ取得
+    public List<RankData> getRanking()
+    {
+        return rankDatas;
+    }
+
+    //ランキングデータ設定
+    public bool setRankData(RankData data)
+    {
+        int num = 0;
+        foreach (var rank in rankDatas)
+        {
+            if (rank.score >= data.score)
+            {
+                rankDatas[num] = data;
+                return true;
+            }
+            num++;
+        }
+
+        return false;
+    }
+
+}
