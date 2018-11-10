4abf53fe561a59f369d39405aba12175beb3fb21
ueno <namakemono1911@gmail.com>
2018-11-10 12:45:27 +0900

    シーン遷移の窓口を用意して

--- Assets/Users/takahiro/Script/SceneManager/sceneManager.cs
+++ Assets/Users/takahiro/Script/SceneManager/sceneManager.cs
@@ -2,34 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class sceneManager : MonoBehaviour {
+public class sceneManager : singletonMonobihavior<sceneManager> {
 
 	public enum SCENE
 	{
 		SCENE_TITLE = 0,
-		SCENE_GAME,
+		SCENE_GAME_NORMAL,
+		SCENE_GAME_BOSS,
+		SCENE_RESULT,
+		SCENE_RANKING,
 		SCENE_MAX
 	}
 
-	private Dictionary<int, string> sceneNames;
+	[SerializeField]
+	private float fadeSecond;
+	[SerializeField]
+	private Image fadeImage;
 
-	private void Awake()
+	[SerializeField]
+	private string[] sceneNames;
+
+	private Dictionary<int, string> sceneNamesIdx;
+	private bool fade;
+	private bool fadeIn;
+    private float fadeSpeed;
+    private SCENE nowScene;
+    private SCENE beforScene;
+    private void Awake()
 	{
 		DontDestroyOnLoad(this.gameObject);
 
 		// シーンと列挙型の関連付け
-		sceneNames = new Dictionary<int, string> {
-			{(int)SCENE.SCENE_TITLE , "title" },
-			{(int)SCENE.SCENE_GAME , "spy" },
+		sceneNamesIdx = new Dictionary<int, string> {
+			{(int)SCENE.SCENE_TITLE , sceneNames[0] },
+			{(int)SCENE.SCENE_GAME_NORMAL , sceneNames[1] },
+			//{(int)SCENE.SCENE_GAME_BOSS , sceneNames[2] },
+			//{(int)SCENE.SCENE_RESULT , sceneNames[3] },
+			//{(int)SCENE.SCENE_RANKING , sceneNames[4] },
 		};
-	}
+
+        beforScene = SCENE.SCENE_TITLE;
+        nowScene = SCENE.SCENE_TITLE;
+
+    }
+
+    private void Start()
+    {
+        fadeSpeed = 1.0f / fadeSecond;
+    }
+
+    private void Update()
+	{
+        Debug.Log(fade);
+
+        // fade
+        if (fade != true)
+        {
+            return;
+        }
+
+        Color color = fadeImage.color;
+
+        if (fadeIn)
+        {
+            color.a -= fadeSpeed;
+
+            if (color.a <= 0.0f)
+            {
+                color.a = 0.0f;
+                fade = false;
+                fadeIn = false;
+            }
+
+        }
+        else
+        {
+            color.a += fadeSpeed;
+
+            if (color.a >= 1.0f)
+            {
+                color.a = 1.0f;
+                fadeIn = true;
+                SceneManager.LoadScene(sceneNames[(int)nowScene]);
+            }
+        }
+
+        fadeImage.color = new Color(color.r, color.g, color.b, color.a);
+
+    }
 
 	public void SceneChange(SCENE scene)
 	{
-		SceneManager.LoadScene(sceneNames[(int)scene]);
+        beforScene = nowScene;
+        nowScene = scene;
+        fade = true;
+        fadeIn = false;
 	}
-
-
-
 }
---
+++ Assets/Users/takahiro/Script/Test/testSceneManeger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testSceneManeger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            sceneManager.Instance.SceneChange(sceneManager.SCENE.SCENE_GAME_NORMAL);
+        }
+
+    }
+}
