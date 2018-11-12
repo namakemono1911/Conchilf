fb99a02243314c66eac176a6fa4ece56deac918a
DESKTOP-0KV59T7\Shun_ <gerut501@gmail.com>
2018-11-12 14:32:57 +0900

    EnemySceneManage,EnemyCreaterを更新

--- Assets/Users/sakai/Script/EnemyManager.cs
+++ Assets/Users/sakai/Script/EnemyManager.cs
@@ -3,8 +3,8 @@
 //  Title   : エネミーマネージャ
 //  Auther  : Shun Sakai 
 //  Date    : 2018/10/10
-//  Update  : リファクタリング
-//  Memo    : 
+//  Update  : 2018/11/12
+//  Memo    : シーン事に生成する機能を追加、ウェーブ単位でのアクティブ切り替え追加
 //
 ///////////////////////////////////////////////
 using UnityEngine;
@@ -31,6 +31,8 @@ public class EnemyManager : MonoBehaviour {
 
     private List<string[]>          CsvDate;            // csvデータ
     private List<GameObject>        WaveDate;           // ウェーブ毎のデータ
+    private List<GameObject>        SceneDate;           // ウェーブ毎のデータ
+
     // インスペクター入力忘れ防止
     private void Awake()
     {
@@ -46,16 +48,17 @@ public class EnemyManager : MonoBehaviour {
         // リスト作成
         CsvDate  = new List<string[]>();
         WaveDate = new List<GameObject>();
+        SceneDate = new List<GameObject>();
     }
 
-    // Use this for initialization
+    // 初回処理
     void Start () {
 
         // csvデータリストの初期化
         CsvDate.Clear();        
     }
 	
-	// Update is called once per frame
+	// 更新処理
 	void Update () {
 		
 	    // デバッグ
@@ -84,6 +87,137 @@ public class EnemyManager : MonoBehaviour {
 			Debug.Log ("CSV -> EnemyCreate -> Wave Select");
 		}
 	}
+
+    // シーンデータの作成
+    public bool SceneDataMake(int SceneNum)
+    {
+        // エネミー情報構造体
+        enemy.EnemyInfo buf = new enemy.EnemyInfo();
+
+        // EnemyManager以下のオブジェクトを全削除
+        foreach (Transform child in gameObject.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // csvデータリストの初期化
+        CsvDate.Clear();
+        // csvデータの取得
+        CsvDate = OptionInfo.Csvmanager.CsvDataGet();
+
+        // エネミー生成時に付与するマネージャを取得
+        enemyTypeManager E_Type = OptionInfo.enemyCreater.GetComponent<EnemyCreater>().GetEnemyTypeManager();
+        enemyAnimationManager E_Anim = OptionInfo.enemyCreater.GetComponent<EnemyCreater>().GetEnemyAnimationManager();
+
+        // 解析（Csvデータの末尾まで）
+        for (int index = 0; index < CsvDate.Count; index++)
+        {
+            // シーン番号識別
+            string Scene = CsvDate[index][(int)EnemyAnalyze.Enemy_Scene];
+
+            // シーン数確認
+            if (int.Parse(Scene) == SceneNum)
+            {
+                GameObject obj = null;
+
+                // 識別番号取得
+                int id = int.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Type]);
+                buf.MODEL_NUMBER = (enemyTypeManager.ENEMY_TYPE)Enum.ToObject(typeof(enemyTypeManager.ENEMY_TYPE), id);
+
+                // 移動間秒数取得
+                buf.MOVE_SECOND = float.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_MoveSec]);
+
+                // 生成座標を取得
+                Vector3 pos;
+                pos.x = float.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Pos_x]);
+                pos.y = float.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Pos_y]);
+                pos.z = float.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Pos_z]);
+                buf.ENEMY_POS = pos;
+
+                // 移動先情報を取得
+                Vector3 Mpos;
+                Mpos.x = float.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Move_x]);
+                Mpos.y = float.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Move_y]);
+                Mpos.z = float.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Move_z]);
+                buf.ENEMY_MOVE_POS = Mpos;
+
+                // ウェーブ数を取得
+                buf.WAVE_NUMBER = int.Parse(CsvDate[index][(int)EnemyAnalyze.Enemy_Wave]);
+
+                // エネミー生成
+                obj = Instantiate(OptionInfo.Enemy00, pos, Quaternion.identity);
+
+                // 生成フラグをたてる
+                obj.GetComponent<enemy>().createMeFrag();
+
+                // エネミー情報セット
+                obj.GetComponent<enemy>().setEnemyInfo(buf);
+
+                // エネミータイプマネージャー
+                obj.GetComponent<enemy>().setEnemyTypeManager(E_Type);
+                // アニメマネージャをセット
+                obj.GetComponent<enemyAnimation>().setEnemyAnimManager(E_Anim);
+
+                // エネミーの追加
+                SceneDate.Add(obj);
+
+                // 子供として追加
+                obj.transform.parent = transform;
+
+                // Activeをfalseに
+                obj.SetActive(false);
+            }
+
+        }
+
+        if (SceneDate.Count == 0) { return false; }
+        return true;
+    }
+
+    // 指定ウェーブのエネミーアクティブ化
+    public bool WaveEnemyActiveOnTrue(int WaveIndex)
+    {
+        if(SceneDate == null){return false;}
+
+        // 変数
+        int WaveNum = 0;
+        int Count   = 0;
+
+        // 現在読み込んでいるシーンデータから指定したウェーブのエネミーを生成
+        for(int i = 0; i < SceneDate.Count;i++)
+        {
+            // ウェーブデータの確認
+            WaveNum = SceneDate[i].GetComponent<enemy>().getEnemyInfo().WAVE_NUMBER;
+            
+            // 指定したウェーブ数か
+            if(WaveNum == WaveIndex)
+            {
+                // カウントアップ
+                Count++;
+                // 該当ゲームオブジェクトをActiveにする？
+                SceneDate[i].SetActive(true);
+
+            }
+
+        }
+
+        if( Count == 0) { return false; }
+        return true;
+        
+    }
+
+    // 指定シーンのエネミーに生き残りがいるか
+    public bool SceneEnemyAllDead()
+    {
+        // シーンデータの確認
+        if (SceneDate == null) { return　false; }
+
+        // シーンデータリストが空だったら全滅
+        if (SceneDate.Count == 0) { return true; }
+        
+        return false;
+    }
+
 
     // ウェーブデータ作成
     public void WaveDateMake(int WaveNum)
@@ -164,4 +298,5 @@ public class EnemyManager : MonoBehaviour {
 
         // 全オブジェクトをnonactiveに
     }
+    
 }
---
+++ Assets/Users/sakai/Script/EnemySceneManager.cs
@@ -0,0 +1,167 @@
+﻿///////////////////////////////////////////////
+//
+//  Title   : エネミーシーンマネージャ
+//  Auther  : Shun Sakai 
+//  Date    : 2018/11/12
+//  Update  : 
+//  Memo    : 
+//
+///////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// エネミーシーン管理マネージャ
+public class EnemySceneManager : MonoBehaviour {
+
+    // シリアライズ
+    [System.Serializable]
+    class Option
+    {
+        public EnemyManager Enemy_manager;  // エネミーマネージャ
+    }
+
+    [SerializeField]
+    private Option SerializeMember; // インスペクタ上のメンバ
+
+    // 内部メンバ
+    private int CurrentSceneNum;    // 現在のシーン番号
+    private int CurrentWaveNum;     // 現在のウェーブ番号
+
+    // 初期処理
+    private void Awake()
+    {
+        if (SerializeMember.Enemy_manager == null)
+        {
+            // エネミーマネージャを探索
+            SerializeMember.Enemy_manager = GameObject.Find("EnemyManager").GetComponent<EnemyManager>();
+            if (SerializeMember.Enemy_manager == null)
+            {
+                Debug.LogError("Error [GameObject.Find] : EnemyManager");
+                return;
+            }
+
+            // 初期化
+            CurrentSceneNum = 0;
+            CurrentWaveNum  = 0;
+
+        }
+    }
+
+
+    // 初回処理
+    void Start () {
+
+        // 初期化
+        CurrentSceneNum = 0;
+        CurrentWaveNum = 0;
+        
+    }
+
+    // 更新処理
+    void Update () {
+		
+	}
+
+    /////////////////////////////
+    //  外部用メソッド
+    ////////////////////////////
+
+    public void DebugTest()
+    {
+        StartEnemyScene();
+    }
+
+    public void DebugNextWave()
+    {
+        EnemyWaveNext();
+    }
+
+    public void DebugNextScene()
+    {
+        EnemySceneNext();
+    }
+
+
+    // エネミーシーンの再生開始
+    public bool StartEnemyScene()
+    {
+        // 初期化
+        CurrentSceneNum = 0;
+        CurrentWaveNum = 0;
+
+        // シーンデータの作成
+        if (SceneDateMake() == false) { return false; }
+
+        // ウェーブのアクティブ化
+        if (WaveActive() == false) { return false; }
+
+        return true;
+    }
+
+    // シーン番号の移行(次回シーンデータが存在しない場合は false　を返す)
+    public bool EnemySceneNext()
+    {
+        // ウェーブ番号の初期化
+        CurrentWaveNum = 0;
+
+        // 次シーンへ移行
+        CurrentSceneNum++;
+
+        // シーンデータの作成
+        if(SceneDateMake() == false) { return false; }
+
+        // ウェーブのアクティブ化
+        if(WaveActive() == false) { return false; }        
+
+        return true;
+    }
+
+    // ウェーブ番号の移行( 次回ウェーブが存在しない場合は false を返す)
+    public bool EnemyWaveNext()
+    {
+        // 次ウェーブへ移行
+        CurrentWaveNum++;
+
+        // ウェーブのアクティブ化
+        if (WaveActive() == false) { return false; }
+
+        return true;
+    }
+
+
+/////////////////////////////
+//  クラス内メソッド
+////////////////////////////
+
+    // 敵が全滅したか
+    public bool EnemyAllDead()
+    {
+        return SerializeMember.Enemy_manager.SceneEnemyAllDead();
+    }
+    
+    // シーンデータの作成
+    private bool SceneDateMake()
+    {
+        // シーンデータの作成
+        if (SerializeMember.Enemy_manager.SceneDataMake(CurrentSceneNum) == false)
+        {
+            return false;
+        }
+
+        return true;
+    }
+    
+    // ウェーブのアクティブ化
+    private bool WaveActive()
+    {
+        if( SerializeMember.Enemy_manager.WaveEnemyActiveOnTrue(CurrentWaveNum) == false )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
