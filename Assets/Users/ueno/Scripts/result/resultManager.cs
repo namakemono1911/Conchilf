32fa77b26c7d4f061cb24dfaec5d29457c8469e1
ueno <namakemono1911@gmail.com>
2018-11-16 18:40:07 +0900

    シーン遷移の窓口用意

---
+++ Assets/Users/ueno/Scripts/result/resultManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resultManager : MonoBehaviour
+{
+    [SerializeField]
+    rankingManager ranking;             //ランキング
+
+    [SerializeField]
+    textAnimationController animation;  //アニメーション
+
+    private string nextScene;           //次のシーン名
+
+    private void Start()
+    {
+        RankData data = new RankData();
+
+        if (ranking.whetherRankin(data))
+            nextScene = "inputName";
+        else
+            nextScene = "ranking";
+    }
+
+    private void Update()
+    {
+        if (!animation.IsAnimation)
+        {
+
+        }
+    }
+}
