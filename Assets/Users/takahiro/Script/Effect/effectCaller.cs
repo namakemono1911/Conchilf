a90a4b064204b9f5b967289d14304f75e41c3c9a
taktyeMoti <hal.ishiitakahiro@gmail.com>
2018-11-19 17:01:56 +0900

    wordManagerみたいな仕組みでeffectManager完成

---
+++ Assets/Users/takahiro/Script/Effect/effectCaller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectCaller : MonoBehaviour {
+
+	[SerializeField]
+	private int effectNumber;
+
+	[SerializeField]
+	private effectManager effectManager;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.transform.tag == "player")
+		{
+			effectManager.playEffect(effectNumber, this.transform);
+		}
+	}
+}
---
+++ Assets/Users/takahiro/Script/Effect/effectManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] effects;
+
+	public void playEffect(int idx , Transform transform)
+	{
+		GameObject.Instantiate(effects[idx], transform);
+	}
+}
