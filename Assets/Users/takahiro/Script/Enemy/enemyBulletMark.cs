3412db85ee59280c557b29ffcbbf4580c2421094
taktyeMoti <hal.ishiitakahiro@gmail.com>
2018-11-22 15:36:41 +0900

    敵の弾痕をいい感じにした

---
+++ Assets/Users/takahiro/Script/Enemy/enemyBulletMark.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBulletMark : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject enemyBulletMarkObj;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "enemyBullet")
+		{
+			GameObject.Instantiate(enemyBulletMarkObj, this.transform);
+		}
+	}
+}
