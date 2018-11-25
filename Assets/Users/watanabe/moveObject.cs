d6c456a9279fa929743773deda582f531181c8aa
xXwetanabeXx <gamesrikonn@gmail.com>
2018-11-26 07:31:08 +0900

    GIt100MB制限を知った俺に死角はない

---
+++ Assets/Users/watanabe/moveObject.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveObject : MonoBehaviour {
+
+	[System.Serializable]
+	private enum YukaMaki
+	{
+		Forward,
+		Back,
+		Right,
+		Left,
+		Up,
+		Down,
+	}
+
+	[SerializeField]
+	private YukaMaki move;
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private Rigidbody rb;
+	private int life;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+		if (rb == null)
+			Debug.Log ("toratoratoraにRigitbodyがついていません");
+
+		switch (move)
+		{
+			case YukaMaki.Forward:
+			{
+				rb.AddForce(Vector3.forward * moveSpeed);
+				break;
+			}
+			case YukaMaki.Back:
+			{
+				rb.AddForce(Vector3.back * moveSpeed);
+				break;
+			}
+			case YukaMaki.Left:
+			{
+				rb.AddForce(Vector3.left * moveSpeed);
+				break;
+			}
+			case YukaMaki.Right:
+			{
+				rb.AddForce(Vector3.right * moveSpeed);
+				break;
+			}
+			case YukaMaki.Up:
+			{
+				rb.AddForce(Vector3.up * moveSpeed);
+				break;
+			}
+			case YukaMaki.Down:
+			{
+				rb.AddForce(Vector3.down * moveSpeed);
+				break;
+			}
+		}
+	}
+}
