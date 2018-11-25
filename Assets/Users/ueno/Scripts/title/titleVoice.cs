a0498a37512f3984a2ed480af41f17217f5fd3bc
ueno takumi <namakemono1911@gmail.com>
2018-11-26 01:01:05 +0900

    タイトルに音声設定 ロゴとプレイお礼シーンにアタッチしなおし

---
+++ Assets/Users/ueno/Scripts/title/titleVoice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleVoice : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource voice1;
+
+    [SerializeField]
+    private AudioSource voice2;
+
+    private AudioSource voice;
+    private bool onece = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (Random.Range(0, 100) % 2 == 0)
+            voice = voice1;
+        else
+            voice = voice2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!sceneManager.Instance.isFade() && !onece)
+        {
+            voice.Play();
+            onece = true;
+        }
+	}
+}
