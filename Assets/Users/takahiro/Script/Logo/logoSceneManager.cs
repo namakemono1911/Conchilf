39536d038edbff9e582453fe95056af27ae0f95b
taktye <taktye_tsoyo_basket08@yahoo.co.jp>
2018-11-25 23:32:00 +0900

    ロゴシーン完成 二人のボイスがランダムに鳴る仕様

---
+++ Assets/Users/takahiro/Script/Logo/logoSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logoSceneManager : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip[] voise;
+
+	private int index;
+	// Use this for initialization
+	void Start () {
+		index = (Random.Range(0 , 100)) % 2;
+		GetComponent<AudioSource>().PlayOneShot(voise[index]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 音の終わりを待つ
+		if (!GetComponent<AudioSource>().isPlaying)
+		{
+			sceneManager.Instance.SceneChange(sceneManager.SCENE.SCENE_TITLE);
+		}
+	}
+}
