f243acbd119c0d9c4b6b1eaa75f714f95c14f4cf
ueno <namakemono1911@gmail.com>
2018-11-26 11:08:10 +0900

    ResultBGM二重奏解決

--- Assets/Users/ueno/Scripts/result/ResultBGM.cs
+++ Assets/Users/ueno/Scripts/result/ResultBGM.cs
@@ -7,19 +7,24 @@ public class ResultBGM : MonoBehaviour
     [SerializeField]
     private AudioSource b;
 
-    public static AudioSource bgm;
+    public static AudioSource bgm = null;
 
     // Use this for initialization
     void Start ()
     {
+        if (!ResultBGMManager.setResultBGM(this.gameObject))
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
         bgm = b;
-        ResultBGMManager.setResultBGM(this.gameObject);
         bgm.Play();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+
 	}
 }
--- Assets/Users/ueno/Scripts/result/ResultBGMManager.cs
+++ Assets/Users/ueno/Scripts/result/ResultBGMManager.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class ResultBGMManager
 {
-    static GameObject resultBGM;
+    static GameObject resultBGM = null;
 
-    static public void setResultBGM(GameObject obj)
+    static public bool setResultBGM(GameObject obj)
     {
+        if (resultBGM != null)
+            return false;
+
         Object.DontDestroyOnLoad(obj);
         resultBGM = obj;
+
+        return true;
     }
 
     static public void destoryBGM()
     {
         GameObject.Destroy(resultBGM);
+        resultBGM = null;
     }
 }
