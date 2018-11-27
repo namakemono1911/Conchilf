e198eba88bd54483eb75ef032a13aef8addda70b
ueno takumi <namakemono1911@gmail.com>
2018-11-28 01:42:28 +0900

    ガードUI変更

---
+++ Assets/Users/ueno/Scripts/uiAnimation/SimultaneousPlayback.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimultaneousPlayback : UiAnimationInterface
+{
+    [SerializeField]
+    private UiAnimationInterface[] animations;
+
+    public override void endAnimation()
+    {
+        foreach (var anime in animations)
+            anime.endAnimation();
+    }
+
+    public override void startAnimation()
+    {
+        foreach (var anime in animations)
+            anime.startAnimation();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
